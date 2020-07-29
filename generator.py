80966770b22937ad644ba86ed90e8a53c938873e
retarf <retarf@gmail.com>
2020-07-29 22:00:57 +0200

    Generator update

--- generator.py
+++ generator.py
@@ -1,35 +1,22 @@
 from query import Query
-from mileage import Mileage
+from mileage import Mileage, Route
+
 
 class Generator():
 
-    def __init__(self, _id):
-        self.id = _id
+    def __init__(self):
         self.query = Query()
-        self.mileage = self.query.search(self.id)[0]
 
-    def next_dict(self, m):
-        return {m: self.query.search(start=m.end.name)}
+    def next_mileage(self, _id):
+        m = self.query.search(_id)[0]
+        return self.query.search(start=m.end.name)[0]
 
-    def generate(self):
-        result = []
-        v = self.next_dict(self.mileage)
-        if len(v) > 0:
-            result.append(v)
-            for i in v[self.mileage]:
-                a = self.next_dict(i)
-                if len(a[i]) > 0:
-                    result.append(a)
-                    for y in a[i]:
-                        f = self.next_dict(y)
-                        if len(f[y]) > 0:
-                            result.append(f)
-                            for e in f[y]:
-                                g = self.next_dict(e)
-                                if len(g[e]) > 0:
-                                    result.append(g)
-        return result
+    def generate_route(self, _id):
+        start = self.query.search(_id)[0]
+        li = [start]
+        m = start
+        for i in range(3):
+            m = self.next_mileage(m.id)
+            li.append(m)
 
-
-
-
+        return Route(li)
