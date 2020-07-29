e6e0c253e6f824b0ca5df9f97d419d88a1e6ab2f
retarf <retarf@gmail.com>
2020-07-29 18:29:17 +0200

    Track class added.

--- mileage.py
+++ mileage.py
@@ -35,3 +35,23 @@ class Semaphore():
 
     def __repr__(self):
         return f"{self.name}: {self.state}"
+
+class Track():
+
+    def __init__(self, route, length=4):
+        self.length = length
+        if len(route) > self.length:
+            raise Exception(f"Maximal route length {self.length}")
+        self.route = route
+
+        self.semaphore_names = set()
+        for m in self.route:
+            self.semaphore_names.add(m.start.name)
+            self.semaphore_names.add(m.end.name)
+
+        self.semaphores = {name: Semaphore(name) \
+            for name in self.semaphore_names}
+
+        for m in self.route:
+            m.start = self.semaphores[m.start.name]
+            m.end = self.semaphores[m.end.name]
