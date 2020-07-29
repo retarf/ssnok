81f6b10cf3227f47eb32100c79c90644c0af7f02
retarf <retarf@gmail.com>
2020-07-29 10:39:23 +0200

    interface added.

---
+++ interface.py
@@ -0,0 +1,16 @@
+# RailwayControlPanel.py
+
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def establish_route(_id: int):
+    logger.info(f"trying to establish route {_id}!")
+
+def release_route(_id: int):
+    logger.info(f"trying to release route {_id}!")
+
+def get_signal_state(name: str):
+    return random.choice(["SX", "S1"])
